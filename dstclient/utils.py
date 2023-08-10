22d4a4fd3d286133508dd5cadb5c588c7fe15d49
Basislager Services <services@basislager.fun>
2023-08-10 19:31:05 +0200

    Use webdriver manager

--- dstclient/utils.py
+++ dstclient/utils.py
@@ -22,6 +22,9 @@ from typing import Iterator
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromiumService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
 
 
 @contextlib.contextmanager
@@ -35,7 +38,12 @@ def chromedriver() -> Iterator[webdriver.Chrome]:
         options.add_argument("--disable-dev-shm-usage")  # type: ignore
         options.add_argument("--window-size=1920,1080")  # type: ignore
 
-        driver = webdriver.Chrome(options=options)
+        driver = webdriver.Chrome(
+            service=ChromiumService(
+                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+            ),
+            options=options,
+        )
         driver.implicitly_wait(10)
 
         yield driver
